7c7beafc6f29d6e7e03f0140d2b88a1e2c7bae36
Alonso <alonso.amoreno@live.com>
2020-06-23 10:51:11 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://alonso:qhOVIRfMTvqKuWjd@cluster0-ck7rb.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB
